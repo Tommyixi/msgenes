b3afdc7c390372b783ee1c27f6b6231fedf62cd6
Tommyixi <carpenito.t@gmail.com>
2016-06-30 14:46:01 -0700

    Updates webpage

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,3 @@
 $(document).ready(function(){
   $(document).foundation();
-	$('.research-link').click(function(e){
-	  e.preventDefault()
-	  $('html, body').animate({
-	    scrollTop: $("#" + this.classList[1]).offset().top - 20
-	    },1000 );
-	  })    
 })
